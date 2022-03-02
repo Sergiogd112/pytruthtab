64a96ddc64ccd033a484f8a42c7427052f351576
Sergiogd112 <sergio.gomezdamas@gmail.com>
2022-03-02 17:35:25 +0100

    excercice

--- eq.py
+++ eq.py
@@ -4,11 +4,8 @@ out_names = "D,E"
 
 def eq(inputs):
     A, B, C = inputs
-    D = (
-        A
-        and not ((B or C) and (not (B) or not (C)))
-        and ((not (A) and B) or (not (A) and C))
-        and (not (B) or not (C))
-    )
-    E = not ((not (A) and not (B)) or not (not(A) or not (B) and C)) or (A and B)
-    return [D,E]    
+    X = A and not ((B or C) and (not B or not C))
+    Y = ((not A and B) or (not A and not C)) and (not B or not C)
+    D = X or Y
+    E = not ((not A and not B) or not ((not A or not B) and C)) or (A and B)
+    return [D, E]
