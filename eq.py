f0c1adac9642524e640dd1590ff898a63923fa65
Sergiogd112 <sergio.gomezdamas@gmail.com>
2022-03-02 12:11:46 +0100

    update

--- eq.py
+++ eq.py
@@ -1,6 +1,14 @@
-in_names='A,B,C'
-out_names='W'
+in_names = "A,B,C"
+out_names = "D,E"
+
+
 def eq(inputs):
-    A,B,C=inputs
-    W=(D1 and D0 and A and B) or (D1 and not(D0) and A and B)
-    return [W]
+    A, B, C = inputs
+    D = (
+        A
+        and not ((B or C) and (not (B) or not (C)))
+        and ((not (A) and B) or (not (A) and C))
+        and (not (B) or not (C))
+    )
+    E = not ((not (A) and not (B)) or not (not(A) or not (B) and C)) or (A and B)
+    return [D,E]    
