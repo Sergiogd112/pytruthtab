55d945f7307487331d5676f753a7222d4211539a
Sergiogd112 <sergio.gomezdamas@gmail.com>
2022-03-02 11:37:38 +0100

    cleanup

--- eq.py
+++ eq.py
@@ -1,7 +1,6 @@
-inputs=[0]*4
-output=[0]
 in_names='D1,D0,A,B'
 out_names='W'
 def eq(inputs):
     D1,D0,A,B=inputs
-    return [int((D1 and D0 and A and B) or (D1 and not(D0) and A and B))]
+    W=(D1 and D0 and A and B) or (D1 and not(D0) and A and B)
+    return [W]
--- truthtab.py
+++ truthtab.py
@@ -1,13 +1,44 @@
 from eq import *
+
+
 def main():
-    maxn=2**len(inputs)-1
-    print(' '*(len(str(maxn))+1)+'|'+in_names.replace(',','|')+'||'+out_names.replace(',','|'))
-    for i in range(maxn+1):
-        n=i
+    inn = len(in_names.split(","))
+    maxn = 2 ** inn - 1
+    inputs = [0] * inn
+    header = (
+        "n ".rjust(len(str(maxn)) + 1, " ")
+        + "|"
+        + in_names.replace(",", "|")
+        + "||"
+        + out_names.replace(",", "|")
+    )
+    print(header)
+    print("".join(["+" if x == "|" else "-" for x in header]))
+    for i in range(maxn + 1):
+        n = i
         for j in range(len(inputs)):
-            inputs[j] = n%2
-            n=n//2
-        
-        print((str(i)+' ').rjust(len(str(maxn))+1,' ')+'|'+'|'.join([str(x).center(len(name),' ') for name,x in zip(in_names.split(','),inputs[::-1])])+'||'+'|'.join([str(x).center(len(name),' ') for name,x in zip(out_names.split(','),eq(inputs[::-1]))]))
-if __name__ == '__main__':
-    main() 
+            inputs[j] = n % 2
+            n = n // 2
+        outputs = eq(inputs[::-1])
+        row = (
+            (str(i) + " ").rjust(len(str(maxn)) + 1, " ")
+            + "|"
+            + "|".join(
+                [
+                    str(x).center(len(name), " ")
+                    for name, x in zip(in_names.split(","), inputs[::-1])
+                ]
+            )
+            + "||"
+            + "|".join(
+                [
+                    str(int(x)).center(len(name), " ")
+                    for name, x in zip(out_names.split(","), outputs)
+                ]
+            )
+        )
+        print(row)
+
+
+if __name__ == "__main__":
+    main()
